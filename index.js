13fbf15acfb16cbbb0016e181ac1be71f770b453
cihat-salik <cihat@jotform.com>
2023-01-08 03:46:56 +0300

    Fix async projects process and sync order process

--- index.js
+++ index.js
@@ -2,21 +2,19 @@ import { projectNames } from "./src/constants.js"
 import { getProjects } from "./src/utils.js"
 import { commandList } from "./src/command.js";
 import chalk from "chalk";
-//coming soon
-// import co from "co";
 
-async function sync() {
+(() => {
   const projects = getProjects(projectNames)
 
   console.log(chalk.yellow.white.bold(`Syncing -->  ${JSON.stringify(projects, null, 2)} projects\n`))
 
-  projects.forEach(project => {
-    const { name: projectName, path: projectPath } = project
+  projects.forEach(async project => {
+    const { name, path } = project
 
-    commandList.pull(projectPath, projectName)
-    commandList.push(projectPath, projectName)
-    commandList.sync(projectPath, projectName)
+    console.log(chalk.bgCyan.cyanBright.italic("Project Name --> ", name, " \t\n"))
+
+    await commandList["pull"](path, name)
+    await commandList["push"](path, name)
+    await commandList["sync"](path, name)
   })
-}
-
-sync()
+})()
--- src/command.js
+++ src/command.js
@@ -1,36 +1,39 @@
-import { exec } from "child_process";
+import { exec, execSync } from "child_process";
 import chalk from 'chalk';
 
 import Randoma from 'randoma';
 const random = new Randoma({ seed: 10 })
 
-function execCommand(command, projectName) {
+async function execCommand(command, projectName) {
   const log = console.log;
 
-  exec(command, (err, stdout, stderr) => {
-    if (err) {
-      log(chalk.hex(random.color(0.5).hex().toString()).underline.bold(`Error - ${projectName}: ${err}\n`))
-      return
-    }
-    if (stderr) {
-      log(chalk.hex(random.color(0.5).hex().toString()).underline.bold(`${command}\n - ${projectName}: ${stderr}\n`))
-      return
-    }
-    log(chalk.hex(random.color(0.5).hex().toString()).underline.bold(`Success - ${projectName}: ${stdout}\n`))
-  })
+  log(chalk.yellow.bold(`Running command: ${command} - ${projectName}`))
+
+  try {
+    const stdout = execSync(command).toString()
+
+    log(chalk.hex(random.color(0.5).hex().toString()).underline.bold(`Success - ${projectName}: ${command}\t\n\t`))
+    log(chalk.hex(random.color(0.5).hex().toString()).underline.bold(`Output - ${projectName}: ${stdout}\t\n\t\n\t\n\n`))
+  } catch (error) {
+    log(chalk.hex(random.color(0.5).hex().toString()).underline.bold(`Error - ${projectName}: ${error}\t\n\t\n\t\n\n`))
+    return
+  }
 }
 
 export const commandList = {
-  pull: (projectPath, projectName) => {
+  pull: async (projectPath, projectName) => {
     const pullCommand = `cd ${projectPath} && git pull upstream master`
-    execCommand(pullCommand, projectName)
+    // const testCommand = "echo 'pull: 1'"
+    await execCommand(pullCommand, projectName)
   },
-  push: (projectPath, projectName) => {
+  push: async (projectPath, projectName) => {
     const pushCommand = `cd ${projectPath} && git push origin master`
-    execCommand(pushCommand, projectName)
+    // const testCommand = "echo 'push: 2'"
+    await execCommand(pushCommand, projectName)
   },
-  sync: (projectPath, projectName) => {
+  sync: async (projectPath, projectName) => {
     const syncCommand = `cd ${projectPath} && ./sync`
-    execCommand(syncCommand, projectName)
+    // const testCommand = "echo 'sync: 3'"
+    await execCommand(syncCommand, projectName)
   }
 }
